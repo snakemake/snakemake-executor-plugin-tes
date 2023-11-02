72ea9ca504ae8d9090823d745731815c42b75a17
Johannes Koester <johannes.koester@uni-due.de>
2023-11-02 23:19:58 +0100

    imply shared FS

--- snakemake_executor_plugin_tes/__init__.py
+++ snakemake_executor_plugin_tes/__init__.py
@@ -71,7 +71,7 @@ common_settings = CommonSettings(
     # Define whether your executor plugin implies that there is no shared
     # filesystem (True) or not (False).
     # This is e.g. the case for cloud execution.
-    implies_no_shared_fs=False,
+    implies_no_shared_fs=True,
     pass_default_storage_provider_args=True,
     pass_default_resources_args=True,
     pass_envvar_declarations_to_cmd=True,
