e533d5cb1edf11c6d1955004911aead6507e85db
Johannes Koester <johannes.koester@uni-due.de>
2023-11-03 08:29:21 +0100

    use minio play for testing

--- tests/tests.py
+++ tests/tests.py
@@ -4,15 +4,28 @@ from snakemake_executor_plugin_tes import ExecutorSettings
 from snakemake_interface_executor_plugins.settings import ExecutorSettingsBase
 
 
-class TestWorkflowsBase(snakemake.common.tests.TestWorkflowsLocalStorageBase):
+class TestWorkflowsBase(snakemake.common.tests.TestWorkflowsMinioPlayStorageBase):
     __test__ = True
 
     def get_executor(self) -> str:
         return "tes"
+    
+    def get_assume_shared_fs(self) -> bool:
+        return False
 
     def get_executor_settings(self) -> Optional[ExecutorSettingsBase]:
         return ExecutorSettings(
             url="http://localhost:8000",
             user="funnel",
             password="funnel_password",
+        )
+
+    def get_remote_execution_settings(
+        self,
+    ) -> snakemake.settings.RemoteExecutionSettings:
+        return snakemake.settings.RemoteExecutionSettings(
+            seconds_between_status_checks=10,
+            envvars=self.get_envvars(),
+            # TODO remove once we have switched to stable snakemake for dev
+            container_image="snakemake/snakemake:latest",
         )
